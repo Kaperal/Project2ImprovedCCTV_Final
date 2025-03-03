ce3879b4d54cefb0f59d100d38b47ff13aff7930
AlwinPalma <alwin.palma02@gmail.com>
2025-03-03 16:03:09 +0800

    March 3

--- audio_detectV3.py
+++ audio_detectV3.py
@@ -55,7 +55,7 @@ class AudioDetection:
 
         return X, y
 
-    def train_model(self, X, y, save_path="trained_model.pkl"):
+    def train_model(self, X, y, save_path="trained_model2.pkl"):
         """Train an SVM model with the given data and save the model."""
         # Train-test split
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
